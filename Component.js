100c1987a6a2dc7ba5cd172ff77b3636addff2f1
Konstantin Tatosov <tatosovko@gmail.com>
2017-07-17 16:32:11 +0300

    #PDM2-122 общая структура приложения

---
+++ Component.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+        "sap/ui/core/UIComponent"
+    ],
+    function (UIComponent) {
+
+        return UIComponent.extend("ilim.pdm2.vacation_planning.Component", {
+
+            metadata: {
+                manifest: "json"
+            },
+
+            init: function () {
+
+                UIComponent.prototype.init.apply(this, arguments);
+
+
+            }
+        });
+    }
+);
