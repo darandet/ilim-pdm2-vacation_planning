e9efcf5dea4e5a5a406e677dd5135e99281c82cc
Александр <30497121+aafrolov84@users.noreply.github.com>
2017-12-06 18:43:37 +0300

    Фильтрация на трех закладках

--- utils/managerController.js
+++ utils/managerController.js
@@ -14,10 +14,19 @@ sap.ui.define([
                 this.oModel = {};
                 this.oWhenPeriodIsLoaded = {};
                 this.aSelectedEmplCntx = [];
-                this.aDepartments = [];
-                this.aStatuses = [];
-                this.aManagers = [];
-                this.sSearchline = "";                    
+                this.aPDepartments = [];
+                this.aPStatuses = [];
+                this.aPManagers = [];
+                this.aTDepartments = [];
+                this.aTStatuses = [];
+                this.aTManagers = [];
+                this.aCDepartments = [];
+                this.aCStatuses = [];
+                this.aCManagers = [];
+                this.sTab = "plan";
+                this.sPSearchline = "";
+                this.sTSearchline = "";
+                this.sCSearchline = "";                  
             },
 
             setCurrentYear: function (sYear) {
@@ -25,30 +34,80 @@ sap.ui.define([
             },
                 
             setSearchline: function (sVal) {
-                this.sSearchline = sVal;
+                switch (this.sTab) {
+                  case "plan":
+                    this.sPSearchline = sVal.toUpperCase();
+                    break;
+                  case "tran":
+                    this.sTSearchline = sVal.toUpperCase();
+                    break;
+                  case "conf":
+                    this.sCSearchline = sVal.toUpperCase();
+                    break;
+                }
             },
 
             addStatusVal: function (sVal) {
-                this.aStatuses.push(sVal);
+                switch (this.sTab) {
+                  case "plan":
+                    this.aPStatuses.push(sVal);
+                    break;
+                  case "tran":
+                    this.aTStatuses.push(sVal);
+                    break;
+                  case "conf":
+                    this.aCStatuses.push(sVal);
+                    break;
+                }
             },
 
             addDepartVal: function (sVal) {
-                this.aDepartments.push(sVal);
+                switch (this.sTab) {
+                  case "plan":
+                    this.aPDepartments.push(sVal);
+                    break;
+                  case "tran":
+                    this.aTDepartments.push(sVal);
+                    break;
+                  case "conf":
+                    this.aCDepartments.push(sVal);
+                    break;
+                }
             },
 
             addManagerVal: function (sVal) {
-                this.aManagers.push(sVal);
+                switch (this.sTab) {
+                  case "plan":
+                    this.aPManagers.push(sVal);
+                    break;
+                  case "tran":
+                    this.aTManagers.push(sVal);
+                    break;
+                  case "conf":
+                    this.aCManagers.push(sVal);
+                    break;
+                }
             },
 
             clearFilters: function () {
-                this.aDepartments = [];
-                this.aStatuses = [];
-                this.aManagers = [];
+                switch (this.sTab) {
+                  case "plan":
+                    this.aPDepartments = [];
+                    this.aPStatuses = [];
+                    this.aPManagers = [];
+                    break;
+                  case "tran":
+                    this.aTDepartments = [];
+                    this.aTStatuses = [];
+                    this.aTManagers = [];
+                    break;
+                  case "conf":
+                    this.aCDepartments = [];
+                    this.aCStatuses = [];
+                    this.aCManagers = [];
+                    break;
+                }
             },
-
-            addStatusVal: function (sVal) {
-                this.aStatuses.push(sVal);
-            },                
                 
             setOnlySubord: function (sSubord) {
                 this.onlySubord = sSubord;
@@ -62,41 +121,75 @@ sap.ui.define([
                 return this.selectedYear;
             },
 
+            setCurrentTab: function (sTab) {
+                this.sTab = sTab;
+            },                
+                
+            getCurrentTab: function () {
+                return this.sTab;
+            },                
+
             getComplexFilter: function () {
 
                 var aFilters = [];
+                var aStatuses = [];
+                var aDepartments = [];
+                var aManagers = [];
+                var sSearch = "";
                 var EmployeeFilter;
+
+                switch (this.sTab) {
+                  case "plan":
+                    aStatuses    = this.aPStatuses;
+                    aDepartments = this.aPDepartments;
+                    aManagers    = this.aPManagers;
+                    sSearch      = this.sPSearchline;
+                    break;
+                  case "tran":
+                    aStatuses    = this.aTStatuses;
+                    aDepartments = this.aTDepartments;
+                    aManagers    = this.aTManagers;
+                    sSearch      = this.sTSearchline;
+                    break;
+                  case "conf":
+                    aStatuses    = this.aCStatuses;
+                    aDepartments = this.aCDepartments;
+                    aManagers    = this.aCManagers;
+                    sSearch      = this.sCSearchline;
+                    break;
+                }
+
                 var filter = new Filter("PlanYear", sap.ui.model.FilterOperator.EQ, this.getCurrentYear());
                 aFilters.push(filter);
                 filter = new Filter("OnlySubord", sap.ui.model.FilterOperator.EQ, this.getOnlySubord());
                 aFilters.push(filter);
 
-                if (this.sSearchline.length > 0) {
-                    if (/^\d+$/.test(this.sSearchline)) {
-                        filter = new Filter("EmployeeId", sap.ui.model.FilterOperator.Contains, this.sSearchline);
+                if (sSearch.length > 0) {
+                    if (/^\d+$/.test(sSearch)) {
+                        filter = new Filter("EmployeeId", sap.ui.model.FilterOperator.Contains, sSearch);
                     } else {
-                        filter = new Filter("EmployeeName", sap.ui.model.FilterOperator.Contains, this.sSearchline);
+                        filter = new Filter("EmployeeNameUp", sap.ui.model.FilterOperator.Contains, sSearch);
                     }
                     aFilters.push(filter);
                 }
 
-                if (this.aStatuses.length > 0) {
-                    for (var i = 0; i < this.aStatuses.length; i++) {
-                        filter = new Filter("PlanStatus", sap.ui.model.FilterOperator.EQ, this.aStatuses[i]);
+                if (aStatuses.length > 0) {
+                    for (var i = 0; i < aStatuses.length; i++) {
+                        filter = new Filter("PlanStatus", sap.ui.model.FilterOperator.EQ, aStatuses[i]);
                         aFilters.push(filter);
                     }
                 }
 
-                if (this.aDepartments.length > 0) {
-                    for (var i = 0; i < this.aDepartments.length; i++) {
-                        filter = new Filter("OrgunitId", sap.ui.model.FilterOperator.EQ, this.aDepartments[i]);
+                if (aDepartments.length > 0) {
+                    for (var i = 0; i < aDepartments.length; i++) {
+                        filter = new Filter("OrgunitId", sap.ui.model.FilterOperator.EQ, aDepartments[i]);
                         aFilters.push(filter);
                     }
                 }
 
-                if (this.aManagers.length > 0) {
-                    for (var i = 0; i < this.aManagers.length; i++) {
-                        filter = new Filter("Chper", sap.ui.model.FilterOperator.EQ, this.aManagers[i]);
+                if (aManagers.length > 0) {
+                    for (var i = 0; i < aManagers.length; i++) {
+                        filter = new Filter("Chper", sap.ui.model.FilterOperator.EQ, aManagers[i]);
                         aFilters.push(filter);
                     }
                 }
